9551a3d215e3f304f7fdfa27dc6b74f4955432b1
Chilledtan <tpanibhate31@gmail.com>
2023-10-07 20:21:02 +0530

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // import Menu from "./Menu.svelte";
+    import MeNu from "./MeNu.svelte"
+    import Bottom_nav from "./Bottom_nav.svelte";
+    import Table from "./Table.svelte";
+</script>
+<!-- Menu -->
+<MeNu />
+
+<!-- LADLE TABLE -->
+<Table />
+
+<!-- Bottom_nav BAR -->
+<Bottom_nav />
